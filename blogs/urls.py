831292e94d312a9f57d0bea7c965c314c1ed28c9
ola <atobateleameer@gmail.com>
2024-10-15 10:11:49 +0100

    'allpagesadjusted'

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import BlogViewSet
+from .views import home_view
+from . import views
 
 router = DefaultRouter()
 router.register(r'blogs', BlogViewSet)  # Register the BlogViewSet
 
 urlpatterns = router.urls
+# blogs/urls.py
+
+from django.urls import path
+from .views import BlogListView  # Make sure to import the view
+
+urlpatterns = [
+   path('', views.blog_list, name='blog_list'),
+   path('', home_view, name='home'),
+   path('<int:blog_id>/', views.blog_detail_view, name='blog_detail'), 
+]
